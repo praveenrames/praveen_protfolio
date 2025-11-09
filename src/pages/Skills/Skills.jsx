876236d8fd2dc272383df01b167882e871379c58
praveenrames <rpraveen1dme@gmail.com>
2025-11-09 13:12:19 +0530

    update spelling

--- src/pages/Skills/Skills.jsx
+++ src/pages/Skills/Skills.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Card, CardContent } from "@/components/ui/Card";
+import { Card, CardContent } from "@/components/ui/card";
 import { Badge } from "@/components/ui/badge";
 import IconCloudDemo from "@/components/globe";
 import { Code2, Cpu, Cloud } from "lucide-react";
